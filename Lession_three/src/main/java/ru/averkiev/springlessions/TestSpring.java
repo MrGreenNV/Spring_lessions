3d2c6564607929d390fbacf2bd7eb94c0591856f
mr Green <mrgreen@gmail.com>
2023-04-02 17:42:27 +0300

    changed applicationContext.xml, TestSpring.java

--- Lession_three/src/main/java/ru/averkiev/springlessions/TestSpring.java
+++ Lession_three/src/main/java/ru/averkiev/springlessions/TestSpring.java
@@ -7,9 +7,13 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-        TestBean testBean = context.getBean("testBean", TestBean.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        System.out.println(testBean.getName());
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+
+        System.out.println(musicPlayer.getName() + " " + musicPlayer.getVolume());
 
         context.close();
     }
