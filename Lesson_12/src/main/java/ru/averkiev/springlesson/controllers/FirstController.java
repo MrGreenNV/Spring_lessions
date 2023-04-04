41ff42cf1ecc40c1ba94c74b33c4196d391684e3
mr Green <mrgreen@gmail.com>
2023-04-04 12:39:12 +0300

    add calculator.html change FirstController.java add method calculate()

--- Lesson_12/src/main/java/ru/averkiev/springlesson/controllers/FirstController.java
+++ Lesson_12/src/main/java/ru/averkiev/springlesson/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.averkiev.springlesson.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,11 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -22,5 +25,26 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam("a") int a,
+                            @RequestParam("b") int b,
+                            @RequestParam("action") String action,
+                            Model model) {
+        String res;
+
+        switch (action) {
+            case "multiplication" -> res = (a * b) + "";
+            case "addition" -> res = (a + b) + "";
+            case "subtraction" -> res = (a - b) + "";
+            case "division" -> res = (b != 0 ? (double) a / b : Double.MAX_VALUE) + "";
+            default -> res = "Wrong action!";
+        }
+
+        model.addAttribute("result", res);
+
+
+        return "first/calculator";
     }
 }
