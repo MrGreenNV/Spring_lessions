942ff75199acb2c13a8ee614199989b294bf5444
mr Green <mrgreen@gmail.com>
2023-04-04 11:20:29 +0300

    add FirstController.java, SecondController.java

---
+++ Lesson_11/src/main/java/ru/averkiev/springlesson/controllers/SecondController.java
@@ -0,0 +1,12 @@
+package ru.averkiev.springlesson.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
+    }
+}
