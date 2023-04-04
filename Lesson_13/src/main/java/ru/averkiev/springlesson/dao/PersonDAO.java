0b1b7c9bb29c715153501a634e396e185d830e73
mr Green <mrgreen@gmail.com>
2023-04-04 15:32:42 +0300

    add PeopleController.java, PersonDAO.java, Person.java

---
+++ Lesson_13/src/main/java/ru/averkiev/springlesson/dao/PersonDAO.java
@@ -0,0 +1,31 @@
+package ru.averkiev.springlesson.dao;
+
+import org.springframework.stereotype.Component;
+import ru.averkiev.springlesson.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT = 1;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(PEOPLE_COUNT++, "Tom"));
+        people.add(new Person(PEOPLE_COUNT++, "Bob"));
+        people.add(new Person(PEOPLE_COUNT++, "Bred"));
+        people.add(new Person(PEOPLE_COUNT++, "Jack"));
+        people.add(new Person(PEOPLE_COUNT++, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
