48a39a25004884c2a1e7405014db54413c350944
mr Green <mrgreen@gmail.com>
2023-04-05 23:33:44 +0300

    updated PeopleController.java added methods newPerson and create, Person.java, PersonDAO add method save

--- Lesson_13/src/main/java/ru/averkiev/springlesson/controller/PeopleController.java
+++ Lesson_13/src/main/java/ru/averkiev/springlesson/controller/PeopleController.java
@@ -3,10 +3,9 @@ package ru.averkiev.springlesson.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.averkiev.springlesson.dao.PersonDAO;
+import ru.averkiev.springlesson.model.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -23,14 +22,25 @@ public class PeopleController {
     public String index(Model model) {
         // Получаем всех людей из DAO и передадим на отображение в Thymeleaf на представление
         model.addAttribute("people", personDAO.index());
-        return "/people/index";
+        return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         // Извлекаем одного человека по id из DAO и передаем на отображение в представление
         model.addAttribute("person", personDAO.show(id));
-        return "/people/show";
+        return "people/show";
     }
 
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+//        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
 }
