ec7a387b0ed0f1c18621f36b9218f2a88eb83257
”MrGreenNV” <averkievnv@gmail.com>
2023-04-30 15:08:28 +0300

    create AuthController.java, login.html update SecurityConfig.java

--- Lesson_20/src/main/java/ru/averkiev/springlesson/config/SecurityConfig.java
+++ Lesson_20/src/main/java/ru/averkiev/springlesson/config/SecurityConfig.java
@@ -1,41 +1,42 @@
 package ru.averkiev.springlesson.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import ru.averkiev.springlesson.services.PersonDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-@Component
+@Configuration
 public class SecurityConfig {
 
-//    private final PersonDetailsService personDetailsService;
-//
-//    @Autowired
-//    public SecurityConfig(PersonDetailsService personDetailsService) {
-//        this.personDetailsService = personDetailsService;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/auth/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+        return http.build();
+    }
+
+//    @Bean
+//    public BCryptPasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder();
 //    }
 
-
-//
-//    private final AuthProviderImpl authProvider;
-//
-//    @Autowired
-//    public SecurityConfig(AuthProviderImpl authProvider) {
-//        this.authProvider = authProvider;
-//    }
-//
-//    // Настраивает аутентификацию
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(personDetailsService);
-//    }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
 }
