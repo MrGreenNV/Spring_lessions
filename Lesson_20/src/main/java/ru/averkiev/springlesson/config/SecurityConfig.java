a458606044902e92b2f37d9af2d67e2589091522
”MrGreenNV” <averkievnv@gmail.com>
2023-04-30 13:58:15 +0300

    update SecurityConfig.java

--- Lesson_20/src/main/java/ru/averkiev/springlesson/config/SecurityConfig.java
+++ Lesson_20/src/main/java/ru/averkiev/springlesson/config/SecurityConfig.java
@@ -1,14 +1,26 @@
 package ru.averkiev.springlesson.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import ru.averkiev.springlesson.security.AuthProviderImpl;
+import ru.averkiev.springlesson.services.PersonDetailsService;
 
 @EnableWebSecurity
 @Component
 public class SecurityConfig {
+
+//    private final PersonDetailsService personDetailsService;
+//
+//    @Autowired
+//    public SecurityConfig(PersonDetailsService personDetailsService) {
+//        this.personDetailsService = personDetailsService;
+//    }
+
+
 //
 //    private final AuthProviderImpl authProvider;
 //
@@ -18,7 +30,12 @@ public class SecurityConfig {
 //    }
 //
 //    // Настраивает аутентификацию
-//    protected void configure(AuthenticationManagerBuilder auth) {
-//        auth.authenticationProvider(authProvider);
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(personDetailsService);
 //    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
